cd3245b6f4f2a63be4f328384a83ac40ad444a78
tianjl1986 <112677935+tianjl1986@users.noreply.github.com>
2026-05-02 10:59:04 +0800

    Delete Sources/TinPlayer directory

--- Sources/TinPlayer/DesignTokens.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-struct DesignTokens {
-    static let primaryBlack = Color(white: 0.1)
-    static let offWhite = Color(white: 0.96)
-    static let accentGray = Color(white: 0.6)
-}
